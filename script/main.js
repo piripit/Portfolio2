85f49a282d4869c7869bf8705f807834ee41bbfe
Piripit <fmoriel01@gmail.com>
2024-04-10 03:57:14 +0200

    ss

--- script/main.js
+++ script/main.js
@@ -1,20 +1,44 @@
-var typed = new Typed('#textDefile', {
-  strings: ['FIATUWO', 'Moriel', 'Developpeur Full-Stack'],
-  loop:1,
+var typed = new Typed("#textDefile", {
+  strings: ["FIATUWO", "Moriel", "Developpeur Full-Stack"],
+  loop: 1,
   backSpeed: 100,
-  startDelay:100,
+  startDelay: 100,
   typeSpeed: 50,
 });
-function openModal() {
-  document.getElementById('about-modal').style.display = 'block';
+
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = "block";
 }
 
 function closeModal() {
-  document.getElementById('about-modal').style.display = 'none';
+  document.getElementById("skills-modal").style.display = "none";
+  document.getElementById("about-modal").style.display = "none";
+  document.getElementById("cvModal").style.display = "none";
 }
 
+document
+  .getElementById("about-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    openModal("about-modal");
+  });
 
-document.getElementById('about-link').addEventListener('click', function(event) {
-  event.preventDefault(); 
-  openModal(); 
+document
+  .getElementById("skills-link")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    openModal("skills-modal");
+  });
+document.addEventListener("DOMContentLoaded", function () {
+  const linkedinBtn = document.getElementById("linkedin-btn");
+
+  linkedinBtn.addEventListener("click", function () {
+    window.open(
+      "https://www.linkedin.com/in/y-moriel-fiatuwo-51544a225/",
+      "_blank"
+    );
+  });
+});
+document.getElementById("cvDownloadBtn").addEventListener("click", function () {
+  document.getElementById("cvModal").style.display = "block";
 });

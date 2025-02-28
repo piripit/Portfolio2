7368f18401e10a47fc11f2ff1d785ea86e387001
Piripit <fmoriel01@gmail.com>
2025-02-28 10:36:27 +0100

    ss

--- script/main.js
+++ script/main.js
@@ -11,25 +11,10 @@ function openModal(modalId) {
 }
 
 function closeModal() {
-  document.getElementById("skills-modal").style.display = "none";
-  document.getElementById("about-modal").style.display = "none";
   document.getElementById("cvModal").style.display = "none";
   document.getElementById("projects-modal").style.display = "none";
 }
 
-document
-  .getElementById("about-link")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    openModal("about-modal");
-  });
-
-document
-  .getElementById("skills-link")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    openModal("skills-modal");
-  });
 document.addEventListener("DOMContentLoaded", function () {
   const linkedinBtn = document.getElementById("linkedin-btn");
 
@@ -43,6 +28,3 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById("cvDownloadBtn").addEventListener("click", function () {
   document.getElementById("cvModal").style.display = "block";
 });
-
-
-
